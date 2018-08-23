e56fdb3f2d805ddab9d4d49805180340176a6860
WilliamHuang1995 <shsidforever@gmail.com>
2018-08-23 10:51:08 +0800

    v-bind v-if v-for

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        description: 'A pair of warm, fuzzy socks.'
+        image: './greensocks.jpeg',
+        onSale: true,
+        inventory: 0,
+        details: ["80% cotton","20% polyester","Gender-neutral"],
+        variants: [
+            {
+                variantId:2234,
+                variantColor: "green"
+            },
+            {
+                variantId:1738,
+                variantColor: "blue"
+            }
+        ],
+        sizes: ["tiny","small","normal","big","huge"]
     }
 })
