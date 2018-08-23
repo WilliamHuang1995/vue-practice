7bfb1b0966cc58755455047941b0e8c8ef6d3398
WilliamHuang1995 <shsidforever@gmail.com>
2018-08-23 13:29:57 +0800

    list event class&style

--- main.js
+++ main.js
@@ -1,21 +1,41 @@
 var app = new Vue({
     el: '#app',
     data: {
+        brand: 'Vue Mastery',
         product: 'Socks',
         image: './greensocks.jpeg',
-        onSale: true,
+        inStock: false,
         inventory: 0,
         details: ["80% cotton","20% polyester","Gender-neutral"],
         variants: [
             {
                 variantId:2234,
-                variantColor: "green"
+                variantColor: "green",
+                variantImage: "./greensocks.jpeg"
             },
             {
                 variantId:1738,
-                variantColor: "blue"
+                variantColor: "blue",
+                variantImage: "./bluesocks.jpeg"
             }
         ],
-        sizes: ["tiny","small","normal","big","huge"]
+        // sizes: ["tiny","small","normal","big","huge"],
+        cart: 0
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart(){
+            this.cart -= 1
+        },
+        updateProduct(variantImage){
+            this.image = variantImage
+        }
+    },
+    computed: {
+        title() {
+            return this.brand+ ' '+this.product
+        }
     }
 })
